a30a68613bc0312b738841e2e40a69c93ef4f381
mohitsoni87 <mohit_soni87@ymail.com>
2018-11-07 18:46:02 +0530

    Built by using Unity and C#
    Single Player shooting game

---
+++ Assets/Scripts/BehaviourMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourMover : MonoBehaviour {
+	public float moveSpeed = 3.0f;
+	public float gravity = 9.81f;
+
+	private CharacterController myController;
+	void Start(){
+		myController = gameObject.GetComponent<CharacterController> ();
+	}
+	// Update is called once per frame
+	void Update () {
+		Vector3 movementZ = Input.GetAxis ("Vertical") * Vector3.forward * moveSpeed * Time.deltaTime;
+		Vector3 movementX = Input.GetAxis ("Horizontal") * Vector3.right * moveSpeed * Time.deltaTime;;
+
+
+		//From Local Space to World
+		Vector3 movement = transform.TransformDirection(movementZ + movementX);
+		movement.y -= gravity * Time.deltaTime;
+		Debug.Log ("Vector co-ordinates: " + movement.y);	
+
+		myController.Move (movement);
+	}
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+	public enum motionDirections {Spin, Horizontal, Vertical};
+	public motionDirections motionState = motionDirections.Vertical;
+	public float spinSpeed = 181.0f;
+	public float motionSpeed = 0.1f;
+
+	// Update is called once per frame
+	void Update () {
+		switch(motionState){
+		case motionDirections.Spin:
+			gameObject.transform.Rotate (Vector3.up * spinSpeed * Time.deltaTime);	
+			break;
+		case motionDirections.Horizontal:
+			gameObject.transform.Translate (Vector3.right * Mathf.Cos (Time.timeSinceLevelLoad) * motionSpeed);
+			break;
+		case motionDirections.Vertical:
+			gameObject.transform.Translate (Vector3.up * Mathf.Cos (Time.timeSinceLevelLoad) * motionSpeed);
+			break;
+		}
+}
+}
